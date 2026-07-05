d0dbf1b37210c18a65cb1a766e8f4ead25040ed9
pssongs <64870276+pssongs@users.noreply.github.com>
2026-07-05 05:51:26 +0000

    logging for github actions added

--- weather_etl.py
+++ weather_etl.py
@@ -12,6 +12,13 @@ from config import (
     DB_PASSWORD,
     BASE_URL,
 )
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s: %(message)s"
+)
+
 def create_db_engine():
     return create_engine(
     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
